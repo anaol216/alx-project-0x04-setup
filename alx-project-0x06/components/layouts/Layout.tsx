cf3e83d65b05dbb8dceceb322c7f4e8844f0c3cd
anaol216 <atinafuanaol@gmail.com>
2025-10-07 20:10:29 +0300

    feat: initialize Next.js project with TypeScript, Redux, and Tailwind CSS
    - Added package.json with necessary dependencies and scripts
    - Created _app.tsx and _document.tsx for custom app and document structure
    - Implemented a simple API route in hello.ts
    - Developed a counter application in counter-app.tsx using Redux for state management
    - Set up global styles with Tailwind CSS in globals.css
    - Configured PostCSS for Tailwind CSS
    - Added TypeScript configuration in tsconfig.json
    - Included SVG assets in the public directory

---
+++ alx-project-0x06/components/layouts/Layout.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react"
+import Footer from "./Footer";
+import Header from "./Header";
+
+interface LayoutProps {
+    children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+    return (
+        <>
+            <Header />
+            <main >
+                {children}
+            </main>
+            <Footer />
+        </>
+    );
+};
+
+
+export default Layout;  
